a32ce4b4ec549e12532ad64e47a4928946df3b30
navasnst <navastcheruvetty@gmail.com>
2025-11-24 12:57:53 +0530

    Fix token retrieval and authorization header format

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,10 +7,9 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token =localStorage.getItem("token")
-    
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = Bearer ${token};
   return config;
 });
 
-export default api;
+export default api;
