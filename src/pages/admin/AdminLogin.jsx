e445e1a1b41d7d338dac68ffb8fba0bea93a7aac
navasnst <navastcheruvetty@gmail.com>
2025-12-12 09:49:31 +0530

    Update import path for axios in AdminLogin

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useContext } from "react";
-import api from "axios";
+import api from "../../api/axios";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
